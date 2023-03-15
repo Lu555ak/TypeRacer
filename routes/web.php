9ae0cf759f44b264790286d29f6802cd4a8d3eb0
Lu555ak <grgecicluka@gmail.com>
2023-03-15 18:03:43 +0100

    Authentification setup

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
