2e087a408660fecfbc2666e8fabd2a22e98c0c32
Lxoy <lo14loyt@gmail.com>
2023-03-20 21:23:47 +0100

    Added moving clouds

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,7 +29,6 @@
 
                             <div class="col-md-7">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                <img src='{{asset("images\cloud1.eps")}}'>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
